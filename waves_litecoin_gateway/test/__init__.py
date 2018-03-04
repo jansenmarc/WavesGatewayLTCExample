016da0d90009d64503afa0f2d4ccc5bec5f0e053
henning96 <ger.henning96@gmail.com>
2018-03-04 12:28:13 +0100

    Fixes linting issue and tests autodiscovery.

---
+++ waves_litecoin_gateway/test/__init__.py
@@ -0,0 +1,7 @@
+"""WavesGatewayLTCExample Tests"""
+
+from .test_litecoin_gateway import *
+from .test_litecoin_chain_query_service import *
+from .test_litecoin_gateway import *
+from .test_litecoin_integer_converter_service import *
+from .test_litecoin_transaction_service import *
